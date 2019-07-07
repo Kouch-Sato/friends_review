1a805568cb0920b5a359a80b35cf4d8d9942adf1
Kouch-Sato <redkouchred@gmail.com>
2019-07-07 14:47:18 +0900

    add annotate

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: reviews
+#
+#  id          :bigint           not null, primary key
+#  book_id     :integer
+#  review_type :integer
+#  content     :string(255)
+#  status      :integer
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class ReviewTest < ActiveSupport::TestCase
