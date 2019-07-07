082ebc454289e204a19b932a676a6ed1f87d8f5b
Kouch-Sato <redkouchred@gmail.com>
2019-07-07 14:51:50 +0900

    annotate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
+# == Route Map
+#
+#                          Prefix Verb     URI Pattern                                                                              Controller#Action
+# user_twitter_omniauth_authorize GET|POST /users/auth/twitter(.:format)                                                            users/omniauth_callbacks#passthru
+#  user_twitter_omniauth_callback GET|POST /users/auth/twitter/callback(.:format)                                                   users/omniauth_callbacks#twitter
+#            destroy_user_session DELETE   /logout(.:format)                                                                        devise/sessions#destroy
+#                            root GET      /                                                                                        home#top
+#              rails_service_blob GET      /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+#       rails_blob_representation GET      /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#              rails_disk_service GET      /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+#       update_rails_disk_service PUT      /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#            rails_direct_uploads POST     /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, skip: [:sessions, :passwords, :registrations], controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
