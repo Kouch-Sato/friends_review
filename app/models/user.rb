3d4e4a7f9cf1db2d8124efc8a0a84ce0e7dc4c6a
Kouch-Sato <redkouchred@gmail.com>
2019-07-07 16:57:28 +0900

    create fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,8 +23,7 @@
 #
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :books
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
