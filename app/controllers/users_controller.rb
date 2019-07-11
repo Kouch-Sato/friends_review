4ff82978bdab9f46d896fa0d1c6388b8d795a673
Kouch-Sato <redkouchred@gmail.com>
2019-07-11 20:48:38 +0900

    add book method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @book = @user.books.first
+    @book = @user.book
     @reviews = @book.reviews.checked
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,30 +24,43 @@
 
 class User < ApplicationRecord
   has_many :books
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :omniauthable
 
-    def self.find_for_oauth(auth)
-      user = User.where(uid: auth.uid, provider: auth.provider).first
 
-      unless user
-        user = User.create(
-          email:    User.dummy_email(auth),
-          password: Devise.friendly_token[0, 20],
-          uid:      auth.uid,
-          provider: auth.provider,
-          name:     auth.info.name,
-          nickname: auth.info.nickname,
-          image:    auth.info.image,
-          )
-      end
+  def self.find_for_oauth(auth)
+    user = User.where(
+      uid: auth.uid,
+      provider: auth.provider
+    ).first
 
-      user
+    def book
+      books.first
     end
 
-    private
-    def self.dummy_email(auth)
-      "#{auth.uid}-#{auth.provider}@example.com"
+    unless user
+      user = User.create(
+        email:    User.dummy_email(auth),
+        password: Devise.friendly_token[0, 20],
+        uid:      auth.uid,
+        provider: auth.provider,
+        name:     auth.info.name,
+        nickname: auth.info.nickname,
+        image:    auth.info.image,
+        )
     end
+
+    user
+  end
+
+  private
+  def self.dummy_email(auth)
+    "#{auth.uid}-#{auth.provider}@example.com"
+  end
 
 end
