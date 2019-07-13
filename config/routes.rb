2f7880a30fe2b0e40be675c1001ba4daacd77cf8
Kouch-Sato <redkouchred@gmail.com>
2019-07-13 20:33:55 +0900

    refactor

--- config/routes.rb
+++ config/routes.rb
@@ -16,10 +16,13 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users, skip: [:sessions, :passwords, :registrations], controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users,
+             skip: [:sessions, :passwords, :registrations],
+             controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'logout' => 'devise/sessions#destroy', as: :destroy_user_session
   end
+
   root "home#top"
   resources :books, only: [:show, :edit, :update] do
     resources :reviews, only: [:create, :destroy]
