43800a8706c2ebc8fc37237a2d77fedbe8429f63
Kouch-Sato <redkouchred@gmail.com>
2019-07-14 01:46:48 +0900

    chaange path after signin

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -23,4 +23,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to root_url
     end
   end
+
+  def after_sign_in_path_for(user)
+    user_path(user)
+  end
 end
