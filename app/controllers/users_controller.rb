7afa850cb9c154b15fc286b82c4607cddf305317
kota-shidara <3t.u1.kota@gmail.com>
2019-07-16 00:07:24 +0900

    share the same view between users and books#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = @user.book
+    @review = Review.new
     @reviews = @book.reviews.checked
   end
 end
