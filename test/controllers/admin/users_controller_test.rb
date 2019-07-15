f2a205103a3645bdf8de5534292c044b3155954d
Kouch-Sato <redkouchred@gmail.com>
2019-07-15 20:46:30 +0900

    add test

---
+++ test/controllers/admin/users_controller_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class Admin::UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @current_user = users(:yuri)
+  end
+
+  def test_admin_user_index_with_admin
+    @current_user.uid = ENV.fetch("TWITTER_KOUCH_UID")
+    login_as(@current_user, scope: :user)
+    get admin_users_path
+
+    assert_response :success
+  end
+
+  def test_admin_user_index_with_not_admin
+    login_as(@current_user, scope: :user)
+    get admin_users_path
+
+    assert_redirected_to user_path(@current_user)
+    assert_equal flash[:alert], "不正なアクセスです"
+  end
+
+  def test_admin_user_index_without_login
+    get admin_users_path
+
+    assert_redirected_to root_path
+    # TODO: deviseの日本語化したらflashを書き換える
+    # assert_equal flash[:alert], "不正なアクセスです"
+  end
+end
