c025019a39c0d0af18601ea19c74909e50e2b894
Kouch-Sato <redkouchred@gmail.com>
2019-07-20 16:25:09 +0900

    upgrade admin user index

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,11 @@
 class Admin::UsersController < AdminController
   def index
-    @users = User.all
+    @users = User.all.order("created_at")
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    @review = @user.reviews
+    @review_to_others = @user.review_to_others
   end
 end
