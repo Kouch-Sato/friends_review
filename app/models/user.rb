5840ab7aa45c6dbcfe58d8dc9b720b0b38e9e869
kota-shidara <3t.u1.kota@gmail.com>
2019-07-24 20:38:42 +0900

    remove twitter from access token column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,30 +2,30 @@
 #
 # Table name: users
 #
-#  id                          :bigint           not null, primary key
-#  email                       :string(255)      default(""), not null
-#  encrypted_password          :string(255)      default(""), not null
-#  reset_password_token        :string(255)
-#  reset_password_sent_at      :datetime
-#  remember_created_at         :datetime
-#  sign_in_count               :integer          default(0), not null
-#  current_sign_in_at          :datetime
-#  last_sign_in_at             :datetime
-#  current_sign_in_ip          :string(255)
-#  last_sign_in_ip             :string(255)
-#  created_at                  :datetime         not null
-#  updated_at                  :datetime         not null
-#  provider                    :string(255)
-#  uid                         :string(255)
-#  name                        :string(255)
-#  nickname                    :string(255)
-#  image                       :string(255)
-#  twitter_access_token        :binary(65535)
-#  twitter_access_token_secret :binary(65535)
+#  id                     :bigint           not null, primary key
+#  email                  :string(255)      default(""), not null
+#  encrypted_password     :string(255)      default(""), not null
+#  reset_password_token   :string(255)
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :string(255)
+#  last_sign_in_ip        :string(255)
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  provider               :string(255)
+#  uid                    :string(255)
+#  name                   :string(255)
+#  nickname               :string(255)
+#  image                  :string(255)
+#  access_token           :binary(65535)
+#  access_token_secret    :binary(65535)
 #
 
 class User < ApplicationRecord
-  crypt_keeper :twitter_access_token, :twitter_access_token_secret, encryptor: :active_support, key: ENV.fetch("CRYPT_KEEPER_KEY"), salt: ENV.fetch("CRYPT_KEEPER_SALT")
+  crypt_keeper :access_token, :access_token_secret, encryptor: :active_support, key: ENV.fetch("CRYPT_KEEPER_KEY"), salt: ENV.fetch("CRYPT_KEEPER_SALT")
 
   has_many :books
   has_many :reviews, through: :books
@@ -71,15 +71,15 @@ class User < ApplicationRecord
       name:     sns_account.info.name,
       nickname: sns_account.info.nickname,
       image:    sns_account.info.image,
-      twitter_access_token:        sns_account.credentials.token,
-      twitter_access_token_secret: sns_account.credentials.secret,
+      access_token:        sns_account.credentials.token,
+      access_token_secret: sns_account.credentials.secret,
     )
   end
 
   def update_by_sns_account(sns_account)
     self.update(
-      twitter_access_token:        sns_account.credentials.token,
-      twitter_access_token_secret: sns_account.credentials.secret,
+      access_token:        sns_account.credentials.token,
+      access_token_secret: sns_account.credentials.secret,
     )
   end
 
