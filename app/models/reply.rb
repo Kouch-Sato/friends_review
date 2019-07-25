a27aace0d07c3c60b88481236e3bfc8553767b3f
kota-shidara <3t.u1.kota@gmail.com>
2019-07-26 01:48:25 +0900

    annotated

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: replies
+#
+#  id         :bigint           not null, primary key
+#  review_id  :integer
+#  content    :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Reply < ApplicationRecord
+  belongs_to :review
 end
