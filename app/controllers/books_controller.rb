bacbcc2c202f65a99216ed12766a6458065be8b4
kota-shidara <3t.u1.kota@gmail.com>
2019-07-27 00:30:46 +0900

    delete unused method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,14 +31,6 @@ class BooksController < ApplicationController
     @reply = Reply.new
   end
 
-  ## reviewが1つ1つ承認になったため削除
-  # def update
-  #   @book = Book.find_by(id: params[:id])
-  #   @reviews = @book.reviews.unchecked
-  #   @reviews.map(&:checked!)
-  #   redirect_to user_path(@book.user), notice: "評価をまとめて承認しました"
-  # end
-
   private
   def ensure_book_owner
     @book = Book.find_by(id: params[:id])
