0e559d07bb3861d9546c073d4641448c0df693f3
kota-shidara <3t.u1.kota@gmail.com>
2019-07-27 15:17:40 +0900

    create replies#create and before action

---
+++ app/controllers/replies_controller.rb
@@ -0,0 +1,30 @@
+class RepliesController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+  before_action :ensure_review_owner, only: [:create]
+
+  def create
+    @reply = Reply.new(reply_params)
+    @review = Review.find(@reply[:review_id])
+    @book = @review.book
+    # TODO: Tweetする機能の実装
+    # TODO: replyモデルに保存するときに、urlとハッシュタグを削除する
+    if @reply.save!
+      @review.checked!
+      redirect_to check_book_path(@book), notice: "承認し、コメントをツイートしました。"
+    else
+      redirect_to check_book_path(@book), alert: "承認とツイートに失敗しました。"
+    end
+  end
+
+  private
+  def reply_params
+    params.require(:reply).permit(:review_id, :content)
+  end
+
+  def ensure_review_owner
+    @review = Review.find_by(id: params[:reply][:review_id])
+    unless current_user.id == @review.book.user_id
+      redirect_to user_path(current_user), alert: "権限がありません"
+    end
+  end
+end
