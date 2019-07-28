0ed7c538a37e009ce9868497e3b6669413c6c6c5
kota-shidara <3t.u1.kota@gmail.com>
2019-07-28 11:42:44 +0900

    use data for selector

--- app/assets/javascripts/pages/books.js
+++ app/assets/javascripts/pages/books.js
@@ -2,8 +2,8 @@ $(document).on('turbolinks:load', function() {
   $('.reply-textarea').keyup(function() {
     let review_id = $(this).data('review-id');
     let count     = $(this).val().length;
-    let tweet_btn = $(`#reply-submit-${review_id}`);
-    $(`#length-show-${review_id}`).text("残り" + (140 - count) + "文字");
+    let tweet_btn = $(`[data-reply-tweet-btn = ${review_id}]`);
+    $(`[data-length-show = ${review_id}]`).text("残り" + (140 - count) + "文字");
     if ( count > 140 ) {
       tweet_btn.prop("disabled", true);
     } else {
