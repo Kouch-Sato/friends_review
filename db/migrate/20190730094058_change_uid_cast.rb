1db5929b6df135709fb9425585d670f2520f6f95
Kouch-Sato <redkouchred@gmail.com>
2019-07-30 18:53:34 +0900

    ops

--- db/migrate/20190730094058_change_uid_cast.rb
+++ db/migrate/20190730094058_change_uid_cast.rb
@@ -1,6 +1,6 @@
 class ChangeUidCast < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :uid
+    remove_column :users, :uid, :string
     add_column :users, :uid, :bigint
   end
 end
