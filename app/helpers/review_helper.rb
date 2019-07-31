b5af488fba54167f3f87849ac5561ddc325a6c03
kota-shidara <3t.u1.kota@gmail.com>
2019-07-31 20:23:47 +0900

    make two symble same size

--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,9 +1,9 @@
 module ReviewHelper
   def review_type_symbol(review)
     if review.review_type == "good"
-      return "far fa-circle"
+      return "far fa-circle card__review--good mr-1"
     else
-      return "fas fa-times"
+      return "fas fa-times card__review--bad mr-1"
     end
   end
 
