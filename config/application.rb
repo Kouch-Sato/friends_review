0c76a4856544b545d3458eff8a675797c1405530
kota-shidara <3t.u1.kota@gmail.com>
2019-07-31 18:54:26 +0900

    delete comment out

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module FriendsReview
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
