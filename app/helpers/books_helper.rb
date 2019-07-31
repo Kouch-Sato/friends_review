eb889565707498f9de84362adc90e9ef1d389ff9
kota-shidara <3t.u1.kota@gmail.com>
2019-07-31 23:45:06 +0900

    set reviews#show ogp

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,11 +2,16 @@ module BooksHelper
   require 'base64'
 
   def books_show_meta_tags
+    if @shared_review.present?
+      image_url = reviews_show_og_image_url(@shared_review)
+    else
+      image_url = books_show_og_image_url(@user)
+    end
     {
       title: "#{@user.name}の通信簿",
       og: {
         title: "#{@user.name}の通信簿です",
-        image: books_show_og_image_url(@user),
+        image: image_url,
       }
     }
   end
@@ -21,10 +26,28 @@ module BooksHelper
     ImageUrlGenerator.cloudinary_books_show_url(encoded_avatar, width, y_axis, radius, base_image_version, base_image_name)
   end
 
+  def reviews_show_og_image_url(review)
+    width              = 450
+    font               = "Sawarabi%20Gothic"
+    font_size          = 30
+    text               = create_review_show_text(review)
+    y_axis             = 5
+    base_image_version = "v1564554855"
+    base_image_name    = "review.png"
+    ImageUrlGenerator.cloudinary_reviews_show_url(width, font, font_size, text, y_axis, base_image_version, base_image_name)
+  end
+
   def books_show_twitter_share_url(user)
     url             = request.url
     text            = "#{user.name}の通信簿です！記入したり、結果を覗いたりしよう！"
     hash_tags_array = ["みんなの通信簿"]
     ShareUrlGenerator.twitter_share_url(url, text, hash_tags_array)
+  end
+
+  private
+  def create_review_show_text(review)
+    review_symbol  = review.review_type == "good" ? "◯" : "✕"
+    review_content = review.content
+    return review_symbol + " " + review_content
   end
 end
--- lib/image_url_generator.rb
+++ lib/image_url_generator.rb
@@ -2,4 +2,8 @@ module ImageUrlGenerator
   def self.cloudinary_books_show_url(encoded_avatar, width, y_axis, radius, base_image_version, base_image_name)
     "https://res.cloudinary.com/dsjn31fay/image/upload/l_fetch:#{encoded_avatar},w_#{width},y_#{y_axis},r_#{radius}/#{base_image_version}/friends_review/#{base_image_name}"
   end
+
+  def self.cloudinary_reviews_show_url(width, font, font_size, text, y_axis, base_image_version, base_image_name)
+    "https://res.cloudinary.com/dsjn31fay/image/upload/w_#{width},c_fit,l_text:#{font}_#{font_size}:#{text},y_#{y_axis}/#{base_image_version}/friends_review/#{base_image_name}"
+  end
 end
