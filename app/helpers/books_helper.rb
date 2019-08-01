0e6b0bcc836a4daac87213dced57cc75e5f3d8f7
kota-shidara <3t.u1.kota@gmail.com>
2019-08-01 15:58:03 +0900

    refactor

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -2,7 +2,6 @@ module BooksHelper
   require 'base64'
 
   def books_show_meta_tags
-
     image_url = set_image_url
 
     {
@@ -19,6 +18,7 @@ module BooksHelper
     base_image_version = "v1564473266"
     base_image_name    = "book_top.png"
     option             = "l_fetch:#{encoded_avatar},w_1.8,y_70,r_50"
+
     OgpUrlGenerator.cloudinary_ogp_url(base_image_version, base_image_name, option)
   end
 
@@ -27,6 +27,7 @@ module BooksHelper
     base_image_version = "v1564637855"
     base_image_name    = "review.png"
     option             = "w_430,c_fit,l_text:Sawarabi%20Gothic_30:#{text},y_15"
+
     OgpUrlGenerator.cloudinary_ogp_url(base_image_version, base_image_name, option)
   end
 
@@ -34,6 +35,7 @@ module BooksHelper
     url             = request.url
     text            = "#{user.name}の通信簿です！記入したり、結果を覗いたりしよう！"
     hash_tags_array = ["みんなの通信簿"]
+
     ShareUrlGenerator.twitter_share_url(url, text, hash_tags_array)
   end
 
@@ -41,7 +43,8 @@ module BooksHelper
   def create_review_show_text(review)
     review_symbol  = review.review_type == "good" ? "◯" : "✕"
     review_content = review.content
-    return review_symbol + " " + review_content
+
+    review_symbol + " " + review_content
   end
 
   def set_image_url
@@ -49,6 +52,7 @@ module BooksHelper
     if @shared_review.present?
       image_url = reviews_show_og_image_url(@shared_review)
     end
-    return image_url
+
+    image_url
   end
 end
