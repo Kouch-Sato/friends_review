d3741d4735e405902c755f4853170b8ffa521d20
kota-shidara <3t.u1.kota@gmail.com>
2019-08-02 20:34:08 +0900

    show total count

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,9 @@
 class Admin::UsersController < AdminController
 
-  PER = 10
+  PER = 20
 
   def index
+    @all_users = User.all
     @users = User.all.order("created_at").page(params[:page]).per(PER)
     @reviews = Review.all
   end
